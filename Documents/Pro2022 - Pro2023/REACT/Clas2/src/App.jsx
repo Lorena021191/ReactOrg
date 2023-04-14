0061291a7b1068f20a686652640bb39e59307875
Astrid Lorena Castañeda Vargas <121511350+Lorena021191@users.noreply.github.com>
2023-04-14 18:26:05 -0500

    Update App.jsx

--- Documents/Pro2022 - Pro2023/REACT/Clas2/src/App.jsx
+++ Documents/Pro2022 - Pro2023/REACT/Clas2/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { v4 as uuid } from "uuid"
 import './App.css';
 import Header from "./componentes/Header/Header"
-import Formulario from './componentes/Formulario/Formulario';
+import Formulario from './componentes/formulario/Formulario';
 import MiOrg from './componentes/MiOrg';
 import Equipo from './componentes/Equipo';
 import Footer from './componentes/Footer';
